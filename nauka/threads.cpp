8f8d735a1df76c7fc14384792f8690b6be2a3b12
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-10-13 00:38:16 +0200

    threads
    threads

---
+++ nauka/threads.cpp
@@ -0,0 +1,25 @@
+
+#include <thread>
+#include <iostream>
+using namespace std;
+
+int function1()
+{
+  //sth
+}
+void function2()
+{
+  //sth
+}
+int main()
+{
+  thread first(function1);
+  thread second(function2);
+
+  cout<< first.joinable(); // Czy można połączyć
+  cout<< second.joinable(); // Czy można połączyć
+
+  first.join();
+  second.join();
+
+}
