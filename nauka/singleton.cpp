8e195bc295e6353c93103fa36a0c0cc0f6d2281d
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-10-12 23:43:36 +0200

    Initial commit

---
+++ nauka/singleton.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class singleton{
+private:
+  static singleton* instance;
+  //Tu trzymamy instancję, sprawdzając, czy ona istnieje, będziemy mogli ją przydzielić. Nie możemy dać tutaj "=0", bo musiałaby być const
+
+  Singleton(); //Prywatny konstruktor!!!
+
+public:
+  //function to check whether working
+  void working(){cout<< "yup";}
+  //function to check whether working
+
+  static singleton* getInstance();
+};
+
+singleton *singleton::instance=0;
+
+singleton *singleton::getInstance()
+{
+  if(instance==0)
+  instance = new singleton;
+  return instance;
+}
+
+int main(){
+
+  singleton* r = singleton::getInstance();
+  r->working();
+  singleton* f = singleton::getInstance();
+  f->working();
+
+  //adresy są te same - to jeden obiekt
+  cout<<f<<endl<<r;
+}
