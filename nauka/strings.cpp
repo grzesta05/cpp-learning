a55342027b7436719e03c48c4d9e6a23cb8b2831
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-10-24 12:29:00 +0200

    string functions

--- nauka/strings.cpp
+++ nauka/strings.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
   string str1 = "type";
-  // String functions
+  // String functions!
   bool contain = str1.find("typ") != string::npos; // string find
   transform(str1.begin(), str1.end(), str1.begin(), ::toupper); // transform to upper
   string a = str1.substr(1/*beginning*/, 2 /*end*/); // substr
