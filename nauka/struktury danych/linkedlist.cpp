75ddbf69cf05e404d48791efbeca72f45d1c29b9
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-11-25 16:48:27 +0100

    Linked List implemented

--- nauka/struktury danych/linkedlist.cpp
+++ nauka/struktury danych/linkedlist.cpp
@@ -1,24 +1,105 @@
 #include <iostream>
 #include <bits/stdc++.h>
+
 template<typename x> class node
 {
 public:
   x value;
-  node<x> *prev;
-  node<x> *next;
-  node(x value, node<x> &prev1){
+  node<x> *prev = 0;
+  node<x> *next = 0;
+  node(x value, node<x> *prev1){
     this->value = value;
-    prev = &prev1;
-    prev->next = this;
+    prev = prev1;
+    if(&*prev1)
+    prev1->next = this;
   }
   node(x value) : value(value)  {}
 };
 
+template<typename x> class NodeList
+{
+private:
+  node<x> *head = nullptr;
+  node<x> *tail = nullptr;
+public:
+  int length = 0;
+  node<x> *getHead()
+  {
+    return head;
+  }
+  node<x> *getTail()
+  {
+    return tail;
+  }
+  node<x> *getItem(int index)
+  {
+    if(index >= length)
+    {
+      throw std::invalid_argument("Out of NodeList size");
+    }
+    else{
+      node<x> *pointer = head;
+      for(int i = 0; i < index; i++)
+      {
+        pointer = pointer->next;
+      }
+      return pointer;
+    }
+  }
+  void addItem(x value)
+  {
+    length++;
+    node<x> *obj = new node<x>(value, tail);
+    if(head == 0)
+      head = obj;
+    tail = obj;
+  }
+  void removeItem(int index)
+  {
+    if(index >= length)
+    {
+      throw std::invalid_argument("Out of NodeList size");
+    }
+    else{
+    length--;
+    node<x> *pointer = head;
+    for(int i = 0; i < index; i++)
+    {
+      pointer = pointer->next;
+    }
+    if(pointer == head)
+    {
+      head = head->next;
+    }
+    else{
+      pointer->next->prev = pointer->prev;
+      pointer->prev->next = pointer->next;
+    }
+  delete pointer;
+  }
+}
+  ~NodeList()
+  {
+    node<x> *nextN = head;
+    while(nextN->next != 0)
+    {
+      delete nextN->prev;
+      nextN = nextN->next;
+    }
+    delete nextN;
+    delete head;
+  }
+};
+
+
+
 int main()
 {
-  node<std::string> e1("Mama");
-  node<std::string> e2("Mama2", e1);
-
-  std::cout<< e1.next->value;
-
+  NodeList<int> list;
+  for(int i = 1; i < 101; i++)
+  {
+    list.addItem(i);
+  }
+  for(int i = 0; i < list.length; i++)
+    std::cout<< list.getItem(i)->value << std::endl;
 }
